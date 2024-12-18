958dc62bea54226ef1f95598003eba7df81af1a5
Brnn <raksakul2442@gmail.com>
2024-12-18 14:12:04 +0700

    feat: bake page layout

--- app/test/page.js
+++ app/test/page.js
@@ -1,23 +1,49 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { useEffect, useRef } from 'react';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
 
-export default function BakePage() {
+export default function TestPage() {
+  const model = useLoader(GLTFLoader, './gingerbread/gingerbread3.glb',
+    (loader) => {
+      const dracoLoader = new DRACOLoader();
+      dracoLoader.setDecoderPath('./draco/');
+      loader.setDRACOLoader(dracoLoader);
+    }
+  );
+
+  const modelRef = useRef();
+  const lightRef = useRef(); // Point light reference
+
+  useEffect(() => {
+    if (model && model.scene) {
+      console.log(model);
+      modelRef.current = model.scene;
+    }
+  }, [model]);
+
   return (
-    <div className="h-screen">
-      <Canvas>
-        <ambientLight intensity={0.5} />
-        <spotLight position={[10, 10, 10]} />
-        <mesh position={[-1.5, 0, 0]}>
-          <sphereGeometry args={[0.5, 32, 32]} />
-          <meshStandardMaterial color="orange" />
-        </mesh>
-        <mesh position={[1.5, 0, 0]}>
-          <sphereGeometry args={[0.5, 32, 32]} />
-          <meshStandardMaterial color="blue" />
-        </mesh>
-        <OrbitControls />
-      </Canvas>
-    </div>
+    <Canvas>
+      {/* Ambient light for basic illumination */}
+      <ambientLight intensity={4} />
+
+      {/* Point Light with Helper */}
+      <pointLight
+        ref={lightRef}
+        position={[2, 1, 2]}
+        intensity={100}
+        distance={10}
+        decay={2}
+        color="pink"
+      />
+
+      {/* Render the GLTF Model */}
+      {model && <primitive object={model?.scene} scale={0.35} />}
+
+      <OrbitControls />
+    </Canvas>
   );
 }
