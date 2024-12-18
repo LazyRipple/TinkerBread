62cdf1fabcc2977241b1f3985b7af342b9a1695a
neennera <neennera@gmail.com>
2024-12-18 16:15:51 +0700

    checkpoint: before change prisma schema for itemData_id in GGB

--- app/api/gingerbread/[GGB_id]/route.js
+++ app/api/gingerbread/[GGB_id]/route.js
@@ -41,8 +41,9 @@ export async function GET(request, { params }) {
 
 export async function PATCH(request, { params }) {
   const GGB_id = params.GGB_id
-  const { user_id, receiver_id } = await request.json()
+  const { user_id, receiver_id, item_id, item_message, position } = await request.json()
   try {
+    // check if user valid and not already decorate
     const res = await (await fetch(`${process.env.BASEURL}/api/gingerbreads/${receiver_id}/${user_id}`)).json()
     if (res.message == 'failed') {
       return NextResponse.json(
@@ -55,6 +56,8 @@ export async function PATCH(request, { params }) {
         },
       )
     }
+    console.log(res.data)
+
     if (res.data.is_decorate == 'T') {
       return NextResponse.json(
         {
@@ -66,6 +69,67 @@ export async function PATCH(request, { params }) {
         },
       )
     }
+
+    // TODO : check if GGB_id valid in user
+    // if (!(res.data.GGB1 == GGB_id || res.data.GGB2 == GGB_id || res.data.GGB3 == GGB_id)) {
+    //   return NextResponse.json(
+    //     {
+    //       message: 'failed',
+    //       error: 'GGB id not belong with this receiver user',
+    //     },
+    //     {
+    //       status: 400,
+    //     },
+    //   )
+    // }
+
+    // check if item valid
+    const is_item_id_valid =
+      (await prisma.item.findFirst({
+        where: {
+          id: parseInt(item_id),
+        },
+      })) == null
+    if (is_item_id_valid) {
+      return NextResponse.json(
+        {
+          message: 'failed',
+          error: 'item_id not valid',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    // check if position valid
+    if (!positions.includes(position)) {
+      return NextResponse.json(
+        {
+          message: 'failed',
+          error: 'position not valid',
+        },
+        {
+          status: 400,
+        },
+      )
+    }
+
+    // add new Item data
+    const position_text = position + '_id'
+    const new_item = await prisma.itemData.create({
+      data: {
+        itemId: parseInt(item_id),
+        senderId: user_id,
+        message: item_message,
+      },
+    })
+    const GGB = await prisma.gingerbread.update({
+      where: {
+        id: GGB_id,
+      },
+      data: { [position_text]: new_item.id },
+    })
     return NextResponse.json({
       message: 'success',
     })
@@ -81,3 +145,15 @@ export async function PATCH(request, { params }) {
     )
   }
 }
+
+const positions = [
+  'head1',
+  'left1_hand',
+  'right1_hand',
+  'head2',
+  'left2_hand',
+  'right2_hand',
+  'head3',
+  'left3_hand',
+  'right3_hand',
+]
