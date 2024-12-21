b82a43ec8e6488222324ff972a87eb0276371775
neennera <neennera@gmail.com>
2024-12-21 10:31:53 +0700

    refactor: Bake Provider to other file

--- app/bake/[GGBs_id]/SessionContext.jsx
+++ app/bake/[GGBs_id]/SessionContext.jsx
@@ -1,38 +1,50 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-import { useSession } from 'next-auth/react'
-
+import { createContext, useContext, useEffect, useState } from 'react'
+import { useSession, status } from 'next-auth/react'
 
 const SessionContext = createContext(undefined)
-export const BakeSessionProvider= ({ children }) => {
+export const BakeSessionProvider = ({ children }) => {
   const { data: session, status } = useSession()
-  const [user, setUser] = useState<User | null>(null)
-  const [GGBs, setGGBs] = useState<any | null>(null)
-
+  const [user, setUser] = useState(null)
+  const [GGBs, setGGBs] = useState(null)
+  const [load_status, setLoadStatus] = useState('loading')
   useEffect(() => {
-    const fetchUser = async () => {
+    const fetchUser = async () => {      
       if (status === 'authenticated' && session?.user?.link_id) {
-        const userRes = await fetch(`/api/user/${session.user.link_id}`)
-        const userData = await userRes.json()
-        setUser(userData?.data ?? null)
-
-        if (userData?.data?.GGBs_id) {
-          const GGBRes = await fetch(`/api/gingerbreads/${userData.data.GGBs_id}/me`)
-          const GGBData = await GGBRes.json()
-          setGGBs(GGBData?.data ?? null)
+        try {
+          const userRes = await fetch(`/api/user/${session.user.link_id}`)
+          const userData = await userRes.json()
+          setUser(userData?.data ?? null)
+          console.log("user", userData.data);
+          
+          if (userData?.data?.GGBs_id) {
+            const GGBRes = await fetch(`/api/gingerbreads/${userData.data.GGBs_id}/me`)
+            const GGBData = await GGBRes.json()
+            console.log("GGB", GGBData.data);
+            
+            setGGBs(GGBData?.data ?? null)
+            setLoadStatus('loaded')
+          }
+          
+        } catch (error) {
+          setLoadStatus('error')
         }
       }
     }
-
     fetchUser()
   }, [status, session?.user?.link_id])
+  
 
-  return <BakeSessionProvider.Provider value={{ session, status, user, GGBs }}>{children}</BakeSessionProvider.Provider>
+  return (
+    <SessionContext.Provider value={{ session, status, user, GGBs, load_status }}>
+      {children}
+    </SessionContext.Provider>
+  )
 }
 
 export const useSessionContext = () => {
   const context = useContext(SessionContext)
   if (!context) {
-    throw new Error('useSessionContext must be used within a SessionProvider')
+    throw new Error('useSessionContext must be used within a BakeSessionProvider')
   }
   return context
 }
