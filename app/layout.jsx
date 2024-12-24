d768edc526cbe33536285f2cdaf8f2edb2697191
Brnn <raksakul2442@gmail.com>
2024-12-25 00:21:28 +0700

    feat: audio and icon

--- app/layout.jsx
+++ app/layout.jsx
@@ -12,14 +12,10 @@ export default async function RootLayout({ children }) {
   const session = await getServerSession()
   return (
     <html lang='en' className='antialiased'>
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <link rel="shortcut icon" href="/ggb1_logo.ico" type="image/x-icon" />
+      </head>
       <body>
-        {/* To avoid FOUT with styled-components wrap Layout with StyledComponentsRegistry https://beta.nextjs.org/docs/styling/css-in-js#styled-components */}
-
         <Layout>
           <SessionProvider session={session}>{children}</SessionProvider>
         </Layout>
