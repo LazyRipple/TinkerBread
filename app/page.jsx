5bee52ba80aa22e4a4d368b4d4fa1c73dc097b24
neennera <neennera@gmail.com>
2024-12-24 22:03:27 +0700

    home

--- app/page.jsx
+++ app/page.jsx
@@ -3,27 +3,94 @@ import { Button } from "@/components/Button"
 import Link from "next/link"
 import { GoogleSignInButton } from "@/components/GoogleSignInButton"
 import { useSession } from 'next-auth/react';
+import React, { useState } from 'react';
+import { TutorialModal } from '@/components/Tutorial'
+import { CreditsModal } from '@/components/Credits'
 
 export default function Page() {
+   const [isModalVisible, setModalVisible] = useState(false);
+    const [showModal, setShowModal] = useState(false);
   const {data:session, status} = useSession()
 
   return (
     <>
-      <div className="flex w-full flex-col items-center justify-center space-y-4 pt-20">
-        <h1 className="font-bold">This is Home</h1>
-
+    <video autoPlay muted loop playsInline className="bg-video">
+        <source src="/Tinkerbread_home.mp4" type="video/mp4" />
+    </video>
+    <div className="relative flex size-full min-h-screen flex-col items-center justify-center gap-6 overflow-visible text-white shadow-lg">
+        <div
+            className="my-3 animate-bounce text-4xl font-bold tracking-wider text-white"
+            style={{ animationDuration: '1.5s' }}
+        >
+            Tinkerbread
+        </div>
         {
           status == 'authenticated' ? 
           (
           <Link href={`/bake/me`}>
-            <Button text="My TingerBread" />
+            <button
+            className="w-44 rounded-xl border-2 border-white bg-red-800 px-4 py-2 text-white shadow-lg transition duration-300 hover:scale-105 hover:bg-[#FFD889] hover:text-red-800 hover:shadow-xl focus:outline-none"
+            >My TingerBread</button>
           </Link>
           )
           :
           <GoogleSignInButton />
         }
- 
+        
+        <button
+            onClick={() => setModalVisible(true)}
+            className="w-44 rounded-xl border-2 border-white bg-red-800 px-4 py-2 text-white shadow-lg transition duration-300 hover:scale-105 hover:bg-[#FFD889] hover:text-red-800 hover:shadow-xl focus:outline-none"
+        >
+            Tutorial
+        </button>
+        <button
+            onClick={() => setShowModal(true)}
+            className="w-44 rounded-xl border-2 border-white bg-red-800 px-4 py-2 text-white shadow-lg transition duration-300 hover:bg-[#FFD889] hover:text-red-800"
+        >
+            Credits
+        </button>
       </div>
+
+      <TutorialModal
+          isVisible={isModalVisible}
+          onClose={() => setModalVisible(false)}
+          tutorialPages={tutorialPages}
+      />
+
+      {showModal && <CreditsModal onClose={() => setShowModal(false)} />}
     </>
   )
 }
+
+const tutorialPages = [
+    {
+        topic: 'Make a gingerbread.',
+        description: 'Create your little gingerbread character.',
+        image: '/images/tutorial1.png',
+    },
+    {
+        topic: 'Share your ID.',
+        description: 'Hit ‘Copy ID’ and send it to your friend.',
+        image: '/images/tutorial2.png',
+    },
+    {
+        topic: 'Visit your friend’s kitchen.',
+        description: 'Got their ID? Paste it into the box and check out their kitchen!',
+        image: '/images/tutorial3.png',
+    },
+    {
+        topic: 'Decorate their gingerbread.',
+        description: 'Pick a gingerbread,  some cute accessories, and send a message!',
+        image: '/images/tutorial3.png',
+    },
+    {
+        topic: 'Check your kitchen.',
+        description: 'Messages show up in your kitchen. Click on the accessories to see them!',
+        image: '/images/tutorial3.png',
+    },
+    {
+        topic: 'Bake more gingerbread!',
+        description: 'If all of your gingerbread is fully dressed , hit ‘Bake More’ to make a new one. You can see it by clicking on the gloves!',
+        image: '/images/tutorial3.png',
+    },
+];
--- src/components/Credits.jsx
+++ src/components/Credits.jsx
@@ -1,15 +1,15 @@
 export function CreditsModal({ onClose }) {
     return (
-        <div className="fixed inset-0 flex justify-center items-center bg-black bg-opacity-50">
-            <div className="bg-green-800 border-white border-2 p-8 rounded-2xl shadow-lg text-center max-w-md w-[90%]">
-                <h2 className="text-[#FFD889] text-2xl mb-4 font-bold">Credits</h2>
+        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+            <div className="w-[90%] max-w-md rounded-2xl border-2 border-white bg-green-800 p-8 text-center shadow-lg">
+                <h2 className="mb-4 text-2xl font-bold text-[#FFD889]">Credits</h2>
                 <p className="text-white">This project made by <strong>LazyRipple</strong></p>
                 <p className="text-xl font-medium text-white">
                     <a
                         href="https://github.com/neennera"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-pink-300 underline-none hover:text-pink-500"
+                        className="underline-none text-pink-300 hover:text-pink-500"
                     >
                         neennera <span className="inline">🌊</span>
                     </a>
@@ -18,7 +18,7 @@ export function CreditsModal({ onClose }) {
                         href="https://github.com/Brnn043"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-pink-300 underline-none hover:text-pink-500"
+                        className="underline-none text-pink-300 hover:text-pink-500"
                     >
                         Brnn043 <span className="inline">💫</span>
                     </a>
@@ -26,10 +26,10 @@ export function CreditsModal({ onClose }) {
                 <p className="mb-4 text-white">
                     Visit our repo{" "}
                     <a
-                        href=""
+                        href="https://github.com/LazyRipple/TinkerBread"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-pink-300 underline-none hover:text-pink-500"
+                        className="underline-none text-pink-300 hover:text-pink-500"
                     >
                         here
                     </a>
@@ -40,15 +40,15 @@ export function CreditsModal({ onClose }) {
                         href="https://forms.gle/99UeMnN3zmXJjoAD9"
                         target="_blank"
                         rel="noopener noreferrer"
-                        className="text-pink-300 underline-none hover:text-pink-800"
+                        className="underline-none text-pink-300 hover:text-pink-800"
                     >
                         here
                     </a>
                 </p>
-                <p className="text-[#FFD889] text-lg font-bold border-white">Merry Christmas!</p>
+                <p className="border-white text-lg font-bold text-[#FFD889]">Merry Christmas!</p>
                 <button
                     onClick={onClose}
-                    className="mt-6 w-32 border-2 border-white bg-red-800 hover:bg-[#FFD889] hover:text-red-800 text-white py-2 px-4 rounded-xl shadow-lg transition duration-300"
+                    className="mt-6 w-32 rounded-xl border-2 border-white bg-red-800 px-4 py-2 text-white shadow-lg transition duration-300 hover:bg-[#FFD889] hover:text-red-800"
                 >
                     Close
                 </button>
