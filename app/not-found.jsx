bd4767681283acffe2a942cab939d43967c77645
neennera <neennera@gmail.com>
2024-12-24 22:10:29 +0700

    feat: decorate not-found

--- app/not-found.jsx
+++ app/not-found.jsx
@@ -3,12 +3,22 @@ import Link from "next/link";
 
 export default function NotFoundPage() {
 	return (
-    <div>
-        <h1>Page not found</h1>
-        <p>please check the correction of your url and make sure you are login</p>
-        <Link href="/">
+    <div className="flex size-full flex-col items-center justify-center space-y-4">
+        
+        <video autoPlay muted loop playsInline className="bg-video">
+            <source src="/Tinkerbread_home.mp4" type="video/mp4" />
+        </video>
+        <div className="flex max-w-[80vw] flex-col items-center justify-center space-y-6 rounded-xl bg-black bg-opacity-80 p-8">
+        <div
+            className="my-3 animate-bounce text-2xl font-bold tracking-wider text-white"
+            style={{ animationDuration: '1.5s' }}
+        >
+           This TinkerBread can&apos;t be found !!
+        </div>
+        <p className="text-white">please check the correction of your url and make sure you are login</p>
+        <Link className="w-44" href="/">
             <Button text="Back to Home" />
-        </Link>
+        </Link></div>
     </div>
     )
 }
